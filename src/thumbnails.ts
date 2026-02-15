ae9db2160769d2d1660530695fde4b3c121576cf
Barış DEMİRCİ <hi@338.rocks>
2026-02-15 07:18:34 +0000

    Add portrait image for legends Lady Vera and Rupture

--- src/thumbnails.ts
+++ src/thumbnails.ts
@@ -2,79 +2,83 @@
  * Thumbnails for Brawlhalla legends
  */
 export const thumbnails = {
-	bodvar: "https://cms.brawlhalla.com/c/uploads/2021/07/bodvar.png",
-	cassidy: "https://cms.brawlhalla.com/c/uploads/2021/07/cassidy.png",
-	orion: "https://cms.brawlhalla.com/c/uploads/2021/07/orion.png",
+	bodvar: "https://cms.brawlhalla.com/c/uploads/2021/07/bodvar.png", // 3
+	cassidy: "https://cms.brawlhalla.com/c/uploads/2021/07/cassidy.png", // 4
+	orion: "https://cms.brawlhalla.com/c/uploads/2021/07/orion.png", // 5
 	"lord vraxx":
-		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_VraxxM-1.png",
-	gnash: "https://cms.brawlhalla.com/c/uploads/2021/07/gnash.png",
-	"queen nai": "https://cms.brawlhalla.com/c/uploads/2021/07/nai.png",
-	hattori: "https://cms.brawlhalla.com/c/uploads/2021/07/hattori.png",
-	"sir roland": "https://cms.brawlhalla.com/c/uploads/2021/07/roland.png",
-	scarlet: "https://cms.brawlhalla.com/c/uploads/2021/07/scarlet.png",
-	thatch: "https://cms.brawlhalla.com/c/uploads/2021/07/thatch.png",
-	ada: "https://cms.brawlhalla.com/c/uploads/2021/07/ada.png",
-	sentinel: "https://cms.brawlhalla.com/c/uploads/2021/07/sentinel.png",
-	lucien: "https://cms.brawlhalla.com/c/uploads/2021/07/lucien.png",
-	teros: "https://cms.brawlhalla.com/c/uploads/2021/07/teros.png",
-	brynn: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ValkyrieM.png",
-	asuri: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_CatM.png",
+		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_VraxxM-1.png", // 6
+	gnash: "https://cms.brawlhalla.com/c/uploads/2021/07/gnash.png", // 7
+	"queen nai": "https://cms.brawlhalla.com/c/uploads/2021/07/nai.png", // 8
+	hattori: "https://cms.brawlhalla.com/c/uploads/2021/07/hattori.png", // 10
+	"sir roland": "https://cms.brawlhalla.com/c/uploads/2021/07/roland.png", // 11
+	scarlet: "https://cms.brawlhalla.com/c/uploads/2021/07/scarlet.png", // 12
+	thatch: "https://cms.brawlhalla.com/c/uploads/2021/07/thatch.png", // 13
+	ada: "https://cms.brawlhalla.com/c/uploads/2021/07/ada.png", // 14
+	sentinel: "https://cms.brawlhalla.com/c/uploads/2021/07/sentinel.png", // 15
+	lucien: "https://cms.brawlhalla.com/c/uploads/2021/07/lucien.png", // 9
+	teros: "https://cms.brawlhalla.com/c/uploads/2021/07/teros.png", // 16
+	brynn: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ValkyrieM.png", // 19
+	asuri: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_CatM.png", // 20
 	barraza:
-		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ApocM.png",
-	ember: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ElfM.png",
-	azoth: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_AzothM.png",
-	koji: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SamuraiM.png",
-	ulgrim: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_DwarfM.png",
-	diana: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_MonsterHunterM.png",
-	jhala: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_BarbarianM.png",
-	kor: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_GolemM.png",
+		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ApocM.png", // 21
+	ember: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ElfM.png", // 18
+	azoth: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_AzothM.png", // 23
+	koji: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SamuraiM.png", // 24
+	ulgrim: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_DwarfM.png", // 22
+	diana: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_MonsterHunterM.png", // 25
+	jhala: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_BarbarianM.png", // 26
+	kor: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_GolemM.png", // 28
 	"wu shang":
-		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_MonkM.png",
-	val: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ValM.png",
-	ragnir: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_DragonM.png",
-	cross: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_CrossM.png",
-	mirage: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_AssassinM.png",
-	nix: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ReaperM.png",
-	mordex: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_WerewolfM.png",
-	yumiko: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_KitsuneM.png",
+		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_MonkM.png", // 29
+	val: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ValM.png", // 30
+	ragnir: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_DragonM.png", // 31
+	cross: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_CrossM.png", // 32
+	mirage: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_AssassinM.png", // 33
+	nix: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ReaperM.png", // 34
+	mordex: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_WerewolfM.png", // 35
+	yumiko: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_KitsuneM.png", // 36
 	artemis:
-		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SpaceHunterM.png",
+		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SpaceHunterM.png", // 37
 	caspian:
-		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_Thief.png",
-	sidra: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_Corsair.png",
-	xull: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_BruteM.png",
-	kaya: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_IniuitM-1.png",
-	isaiah: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SoldierM.png",
-	jiro: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ShinobiM.png",
+		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_Thief.png", // 38
+	sidra: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_Corsair.png", // 39
+	xull: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_BruteM.png", // 40
+	kaya: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_IniuitM-1.png", // 42
+	isaiah: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SoldierM.png", // 41
+	jiro: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ShinobiM.png", // 43
 	"lin fei":
-		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_WuxiaM.png",
-	zariel: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_CelestialM.png",
-	rayman: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_RaymanM.png",
-	dusk: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ElfwarM.png",
-	fait: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SpellwitchM.png",
-	thor: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ThorM.png",
-	petra: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_RagefighterM.png",
-	vector: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ActualRobotM.png",
-	volkov: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_VampLordM.png",
-	onyx: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_GargoyleM.png",
-	jaeyun: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SellswordM.png",
-	mako: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ActualSharkM.png",
-	magyar: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_GhostArmorM.png",
-	reno: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_BountyHunterM.png",
-	munin: "https://cms.brawlhalla.com/c/uploads/2021/12/a_Roster_Pose_BirdBardM.png",
+		"https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_WuxiaM.png", // 44
+	zariel: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_CelestialM.png", // 45
+	rayman: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_RaymanM.png", // 46
+	dusk: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ElfwarM.png", // 47
+	fait: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SpellwitchM.png", // 48
+	thor: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ThorM.png", // 49
+	petra: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_RagefighterM.png", // 50
+	vector: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ActualRobotM.png", // 51
+	volkov: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_VampLordM.png", // 52
+	onyx: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_GargoyleM.png", // 53
+	jaeyun: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_SellswordM.png", // 54
+	mako: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_ActualSharkM.png", // 55
+	magyar: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_GhostArmorM.png", // 56
+	reno: "https://cms.brawlhalla.com/c/uploads/2021/07/a_Roster_Pose_BountyHunterM.png", // 57
+	munin: "https://cms.brawlhalla.com/c/uploads/2021/12/a_Roster_Pose_BirdBardM.png", // 58
 	arcadia:
-		"https://cms.brawlhalla.com/c/uploads/2022/03/a_Roster_Pose_FairyQueenM.png",
-	ezio: "https://cms.brawlhalla.com/c/uploads/2022/07/a_Roster_Pose_EzioM.png",
-	tezca: "https://cms.brawlhalla.com/c/uploads/2022/12/a_Roster_Pose_LuchadorM.png",
-	loki: "https://cms.brawlhalla.com/c/uploads/2023/09/Loki_icon-1.png",
-	thea: "https://cms.brawlhalla.com/c/uploads/2023/03/a_Roster_Pose_SpeedsterM.png",
+		"https://cms.brawlhalla.com/c/uploads/2022/03/a_Roster_Pose_FairyQueenM.png", // 59
+	ezio: "https://cms.brawlhalla.com/c/uploads/2022/07/a_Roster_Pose_EzioM.png", // 60
+	tezca: "https://cms.brawlhalla.com/c/uploads/2022/12/a_Roster_Pose_LuchadorM.png", // 63
+	thea: "https://cms.brawlhalla.com/c/uploads/2023/03/a_Roster_Pose_SpeedsterM.png", // 62
 	redraptor:
-		"https://cms.brawlhalla.com/c/uploads/2023/06/a_Roster_Pose_SentaiM.png",
-	vivi: "https://cms.brawlhalla.com/c/uploads/2024/04/Vivilegendpage.jpg",
-	seven: "https://cms.brawlhalla.com/c/uploads/2023/12/roboengineer-rostericon.png",
-	imugi: "https://cms.brawlhalla.com/c/uploads/2024/07/imgui-icon.png",
+		"https://cms.brawlhalla.com/c/uploads/2023/06/a_Roster_Pose_SentaiM.png", // 17
+	loki: "https://cms.brawlhalla.com/c/uploads/2023/09/Loki_icon-1.png", // 27
+	seven: "https://cms.brawlhalla.com/c/uploads/2023/12/roboengineer-rostericon.png", // 61
+	vivi: "https://cms.brawlhalla.com/c/uploads/2024/04/Vivilegendpage.jpg", // 64
+	imugi: "https://cms.brawlhalla.com/c/uploads/2024/07/imgui-icon.png", // 65
 	"king zuva":
-		"https://cms.brawlhalla.com/c/uploads/2024/10/a_Roster_Pose_ZuvaM.png",
-	priya: "https://cms.brawlhalla.com/c/uploads/2025/01/Mobile-icon.png",
-	ransom: "https://cms.brawlhalla.com/c/uploads/2025/06/a_Roster_Pose_CybervirusM.png",
+		"https://cms.brawlhalla.com/c/uploads/2024/10/a_Roster_Pose_ZuvaM.png", // 66
+	priya: "https://cms.brawlhalla.com/c/uploads/2025/01/Mobile-icon.png", // 67
+	ransom: "https://cms.brawlhalla.com/c/uploads/2025/06/a_Roster_Pose_CybervirusM.png", // 68
+	"lady vera":
+		"https://cms.brawlhalla.com/c/uploads/2025/11/a_Roster_Pose_ClericM.png", // 69
+	rupture:
+		"https://cms.brawlhalla.com/c/uploads/2026/01/a_Roster_Pose_DarkheartMonsterM.png", // 70
 };
